5643dd823a78209677f3f0b86fcfcf53664bc41f
Hank <a40954186@gmail.com>
2025-03-26 11:17:27 +0800

    把首頁的原件掛上去

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>🌐 我的網站</h1>
+    <HelloWorld />
+    <Counter />
     <nav>
       <RouterLink to="/">首頁</RouterLink> |
       <RouterLink to="/about">關於我們</RouterLink>
@@ -11,4 +13,8 @@
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
+import HelloWorld from '../components/HelloWorld.vue'
+import Counter from '../components/Counter.vue'
+
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,7 @@
     <div>
       <h2>🏠 歡迎來到首頁！</h2>
     </div>
-    <HelloWorld />
-    <Counter />
 </template>
 
-<script setup>
-import HelloWorld from '../components/HelloWorld.vue'
-import Counter from '../components/Counter.vue'
-</script>
+
   
